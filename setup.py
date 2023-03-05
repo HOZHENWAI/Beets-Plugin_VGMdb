69d842c8be36529a90e22a6896fee5d5e7ec2b5a
olivier <olivier_hozhenwai@yahoo.fr>
2023-03-05 21:38:38 +0100

    update setup for release

--- setup.py
+++ setup.py
@@ -1,13 +1,3 @@
 from setuptools import setup
 
-setup(name="beets-vgmdb",
-      version="1.3.0",
-      description=" Beets VGMdb metadata plugin and collection manager",
-      long_description=open('README.md').read(),
-      author= "HO Zhen Wai Olivier",
-      author_email="hozhenwai@gmail.com",
-      url = "https://github.com/HOZHENWAI/Beets-Plugin_VGMdb",
-      platforms='ALL',
-      packages=['beetsplug'],
-      install_requires=['beets>=1.6.0', 'requests']
-      )
+setup()
